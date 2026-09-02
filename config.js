e2399c7084cb726df43a6c37d7bb3337a00c9ed4
Aryaf-health <aryaf_al@hotmail.com>
2026-09-02 21:41:54 +0300

    Change Supabase publishable key in config
    Updated Supabase publishable key in config.js.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // هذه القيم هي إعدادات الاتصال بالواجهة فقط.
 // لا تضع هنا أي secret/service_role key.
 window.SUPABASE_URL = "https://cdkwgfuvyliqaxckdvz.supabase.co";
-window.SUPABASE_PUBLISHABLE_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_BRKTDWi45OXST7LLQfCHeg_v09PCd70";
